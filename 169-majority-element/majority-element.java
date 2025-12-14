0e47949264f5afbbef8718a15d680b217c9e6b1c
shreya646 <shreya020702005@gmail.com>
2025-12-14 23:18:20 +0530

    Time: 1 ms (99.84%) | Memory: 56 MB (7.61%) - LeetSync

---
+++ 169-majority-element/majority-element.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+       // boyer-moore algo 
+       int ans=nums[0];
+       int count =1;
+       for(int i=1;i<nums.length;i++){
+           if(nums[i]==ans) count++;
+           else count--;
+           if(count==0){ ans=nums[i];
+           count=1;}
+       }
+       
+    return ans;}
+}
