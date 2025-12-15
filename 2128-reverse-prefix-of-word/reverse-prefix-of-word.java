5c0c004dfdfe292a80cd0742f52b3bcf8a5fcfe3
Shreya Jain <shreya020702005@gmail.com>
2025-12-15 22:12:32 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (19.83%) - LeetSync

---
+++ 2128-reverse-prefix-of-word/reverse-prefix-of-word.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String reversePrefix(String word, char ch) {
+        int ind=word.indexOf(ch);
+        if(ind==-1) return word;
+        StringBuilder sb=new StringBuilder(word.substring(0,ind+1));
+        sb.reverse();
+        sb.append(word.substring(ind+1));
+        return sb.toString();
+    }
+}
