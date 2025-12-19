dad1e5aa9d557046f8163f88eda60d21ba80d0ee
Shreya Jain <shreya020702005@gmail.com>
2025-12-19 21:53:57 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (48.79%) - LeetSync

---
+++ 2377-check-if-number-has-equal-digit-count-and-digit-value/check-if-number-has-equal-digit-count-and-digit-value.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean digitCount(String num) {
+    int[] freq=new int[10] ;
+    for(int i=0;i<num.length();i++){
+        int d = num.charAt(i)-'0' ;
+        freq[d]++;}
+       for(int j=0;j<num.length();j++){
+        int y=num.charAt(j)-'0';
+        if(y!=freq[j]) return false;
+       }    
+
+    return true;}
+}
