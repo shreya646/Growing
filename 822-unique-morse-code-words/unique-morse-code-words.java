f81f22aba50531d72ed48771eaaa3a3a2368e0cd
Shreya Jain <shreya020702005@gmail.com>
2025-12-23 16:57:41 +0530

    Time: 2 ms (98.92%) | Memory: 43.6 MB (6.95%) - LeetSync

--- 822-unique-morse-code-words/unique-morse-code-words.java
+++ 822-unique-morse-code-words/unique-morse-code-words.java
@@ -1,11 +1,12 @@
 class Solution {
     public int uniqueMorseRepresentations(String[] words) {
-        String[] arr={".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
-        HashSet <String> ans=new HashSet<>();
-for(String w:words){
+        String[] s={".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
+   HashSet<String> set=new HashSet<>();
+   for(String st:words){
     StringBuilder sb=new StringBuilder();
-    for(char c:w.toCharArray()){
-            sb.append(arr[c-'a']);}
-     ans.add(sb.toString());}
-    return ans.size(); }
+    for(char ch : st.toCharArray()){
+       sb.append(s[ch-'a']);
+    } set.add(sb.toString());}
+
+   return set.size(); }
 }
