1aa075331db255b1837ed5a777384d7853c85522
Shreya Jain <shreya020702005@gmail.com>
2025-12-23 16:10:31 +0530

    Time: 1 ms (80.79%) | Memory: 42.7 MB (43.42%) - LeetSync

--- 290-word-pattern/word-pattern.java
+++ 290-word-pattern/word-pattern.java
@@ -1,16 +1,20 @@
 class Solution {
     public boolean wordPattern(String pattern, String s) {
-        String[] arr= s.split(" ");
-        int n=arr.length;
-        int p=pattern.length();
-        if(n!=p) return false;
-HashMap<Character,String> ptos=new HashMap<>();
-         HashMap<String,Character> stop=new HashMap<>();
-        for(int i=0;i<n;i++){
-            char c=pattern.charAt(i);
-            String t=arr[i];
-       if(ptos.containsKey(c) && !ptos.get(c).equals(t)) return false;
-     if(stop.containsKey(t) && !stop.get(t).equals(c) ) return false;
-      ptos.put(c,t);
-        stop.put(t,c);}
-return true;}}
+        String[] str=s.split(" ");
+        int sp=pattern.length();
+        int ss=str.length;
+        if(ss!=sp) return false;
+        HashMap<Character,String> ptos=new HashMap<>();
+        HashMap<String,Character> stop=new HashMap<>();
+        for(int i=0;i<ss;i++){
+            char ch=pattern.charAt(i);
+            String st=str[i];
+            if(ptos.containsKey(ch) && !ptos.get(ch).equals(st))            return false;
+             if(stop.containsKey(st) &&  !stop.get(st).equals(ch))          return false;
+             ptos.put(ch,st);
+             stop.put(st,ch);
+            
+        }
+
+    return true;}
+}
