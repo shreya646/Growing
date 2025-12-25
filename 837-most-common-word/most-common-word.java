ce582dc876fd84df3a72dab5811d5591cd8463f0
Shreya Jain <shreya020702005@gmail.com>
2025-12-25 10:45:21 +0530

    Time: 12 ms (86.99%) | Memory: 44.6 MB (59.63%) - LeetSync

--- 837-most-common-word/most-common-word.java
+++ 837-most-common-word/most-common-word.java
@@ -1,34 +1,22 @@
 class Solution {
     public String mostCommonWord(String paragraph, String[] banned) {
-       paragraph=paragraph.toLowerCase();
-       Set<String> bannedSet= new HashSet<>(Arrays.asList(banned));
-       Map<String,Integer> freq=new HashMap<>();
-       StringBuilder word=new StringBuilder();
-       for(char c:paragraph.toCharArray()){
-        if(Character.isLetter(c)){
-            word.append(c);}
-            else{
-                if(word.length()>0){
-                    String w=word.toString();
-                    if(!bannedSet.contains(w)){
-                        freq.put(w,freq.getOrDefault(w,0)+1);
-                    } word.setLength(0);
-                }
-            }}
-           if(word.length()>0){
-            String w=word.toString();
-            if(!bannedSet.contains(w)){
-                freq.put(w,freq.getOrDefault(w,0)+1);
-            }
-           }
-           String result="";
-           int max=0; 
-           for(Map.Entry<String,Integer>entry:freq.entrySet()){
-            if(entry.getValue()>max){
-                max=entry.getValue();
-                result=entry.getKey();
-            }
-           } return result;
-        }}
+        String ans="";
+        int maxfreq=0;
+        HashSet<String> set=new HashSet<>();
+        for(String word:banned){
+            set.add(word);
+        }
+        String[] array=paragraph.toLowerCase().split("\\W+");
+        HashMap<String,Integer> map=new HashMap<>();
+        for(String w:array){
+            if(!set.contains(w)){
+        map.put(w,map.getOrDefault(w,0)+1);}}
+        for(String k:map.keySet()){
+            if(map.get(k)>maxfreq){
+                ans=k;
+       maxfreq=map.get(k);}
        
-
+      
+       }
+      
+return ans;}}
