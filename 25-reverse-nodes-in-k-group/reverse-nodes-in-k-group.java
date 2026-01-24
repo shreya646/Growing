c53b21d67ad08de359e82184c4d5d4475a00de19
Shreya Jain <shreya020702005@gmail.com>
2026-01-24 12:12:44 +0530

    Time: 0 ms (100.00%) | Memory: 46.5 MB (24.58%) - LeetSync

--- 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.java
+++ 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.java
@@ -1,28 +1,36 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 class Solution {
     public ListNode reverseKGroup(ListNode head, int k) {
-         if(head==null||head.next==null||k==1){
-        return head;
-    }
-    ListNode start=head;
-     ListNode end=head;
-     int i=0;
-     while(i<k){
-        if(end==null) return head;
-        end=end.next;
-        i++;
-     }
-    ListNode ans= reverse(start,end);
-   start.next= reverseKGroup(end,  k);
-   return ans;
-
+        if(head==null||head.next==null||k==1) return head;
+        ListNode start=head;
+        ListNode end=head;
+        int i=0;
+        while(i<k){
+            if(end==null) return head;
+            end=end.next;
+            i++;
+        }
+       ListNode ans=reverse(start,end);
+       start.next=reverseKGroup(end,k);
+       return ans;
 }
 public static ListNode reverse(ListNode start,ListNode end){
-    ListNode prev=null;
-    ListNode curr=start;
-    while(curr!=end){
-    ListNode temp=curr.next;
-    curr.next=prev;
-    prev=curr;
-    curr=temp;}
-return prev;
-}}
+ListNode p=null;
+ListNode curr=start;
+while(curr!=end){
+ListNode temp =curr.next;
+curr.next=p;
+p=curr;
+curr=temp;
+}
+return p;}
+}
